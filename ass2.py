e9454b880b8a90ba7c60a549471ef8d10be5a4c9
sheenamsehmi <115872687+sheenamsehmi@users.noreply.github.com>
2022-11-05 15:10:38 +0000

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,39 @@
+
+# 2: Write a simple program to print the list in reverse order (you can use any kind of method).
+
+
+# from audioop import reverse
+
+
+# from audioop import reverse
+
+# Marks = [ 1,2,3,4,5,6,7,8]
+
+# # list.reverse()
+
+# Marks.reverse()
+
+# print(Marks)
+
+# 4. Is there any way to change the value of the tuple? If there is then write a program as an example,
+# if not then give the reason with proper examples.
+
+# T = (1,2,3,4,5,6,7)
+
+# lst = list(T)
+
+# lst[1] = '20'
+# T =tuple(lst)
+
+# print(T)
+
+
+
+# Q5: Write a python program to interchange the first and last elements in a list
+
+# num = [10,20,30,40,50]
+
+# num[0] = 50
+# num[-1] = 10
+
+# print(num)
---
+++ ass3.py
@@ -0,0 +1,19 @@
+# 3 Write a program to find if the given number is prime or not (Hint: use the '%' operator).
+  
+
+
+# x = 10
+# y = 4
+
+# if x % y == 2:
+#     print("it is prime number")
+
+# else:
+#     print("not a prime number")
+
+
+
+
+
+
+
